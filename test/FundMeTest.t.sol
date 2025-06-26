609614f68fdb2dbe16e0effc49012f0780778323
vxxxxc <vxxxxc@Serpents-M3.local>
2025-06-27 03:53:04 +0800

    create a fake vm "USER" address by using Forge cheatcode , to carry out further testing cases

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -9,11 +9,16 @@ import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 contract FundMeTest is Test {
     FundMe fundMe;
 
+    address USER = makeAddr("user");
+    uint256 constant SEND_VALUE = 0.1 ether;
+    uint256 constant INIT_VALUE = 10 ether;
+
     function setUp() public returns (FundMe) {
         // fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         // return fundMe;
         DeployFundMe deployFundMe = new DeployFundMe();
         fundMe = deployFundMe.run();
+        vm.deal(USER, INIT_VALUE); // give USER 10 ETH
         return fundMe;
     }
 
@@ -33,5 +38,17 @@ contract FundMeTest is Test {
             uint256 version = fundMe.getVersion();
             assertEq(version, 6);
         }
+    }
+
+    function testFundFailsWithoutEnoughETH() public {
+        vm.expectRevert(); // expect the next line of code is going to revert!
+        fundMe.fund();
+    }
+
+    function testFundUpdatesFundedDataStructure() public {
+        vm.prank(USER);
+        fundMe.fund{value: SEND_VALUE}();
+        uint256 amountFunded = fundMe.getAddressToAmountFunded(USER);
+        assertEq(amountFunded, SEND_VALUE);
     }
 }
