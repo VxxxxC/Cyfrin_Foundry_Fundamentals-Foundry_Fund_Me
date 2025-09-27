f8fff0160d31239ddd984a0385c22cd031c6d4a7
vxxxxc <veper1234@icloud.com>
2025-09-28 01:30:44 +0800

    add forundyup-zksync and zksync test case for optional study

--- test/unit/ZkSyncDevOps.t.sol
+++ test/unit/ZkSyncDevOps.t.sol
@@ -8,7 +8,7 @@ import {FoundryZkSyncChecker} from "lib/foundry-devops/src/FoundryZkSyncChecker.
 
 contract ZkSyncDevOps is Test, ZkSyncChainChecker, FoundryZkSyncChecker {
     // Remove the `skipZkSync`, then run `forge test --mt testZkSyncChainFails --zksync` and this will fail!
-    function testZkSyncChainFails() public skipZkSync {
+    function testZkSyncChainFails() public  {
         address ripemd = address(uint160(3));
 
         bool success;
