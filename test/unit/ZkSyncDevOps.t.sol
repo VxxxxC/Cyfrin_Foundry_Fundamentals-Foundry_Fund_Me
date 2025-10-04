5c994d9affedcc813d3fb2ce31ca8eb01c15adf9
vxxxxc <veper1234@icloud.com>
2025-10-04 15:59:10 +0800

    try fix CI

--- test/unit/ZkSyncDevOps.t.sol
+++ test/unit/ZkSyncDevOps.t.sol
@@ -8,7 +8,7 @@ import {FoundryZkSyncChecker} from "lib/foundry-devops/src/FoundryZkSyncChecker.
 
 contract ZkSyncDevOps is Test, ZkSyncChainChecker, FoundryZkSyncChecker {
     // Remove the `skipZkSync`, then run `forge test --mt testZkSyncChainFails --zksync` and this will fail!
-    function testZkSyncChainFails() public  {
+    function testZkSyncChainFails() public {
         address ripemd = address(uint160(3));
 
         bool success;
@@ -19,12 +19,12 @@ contract ZkSyncDevOps is Test, ZkSyncChainChecker, FoundryZkSyncChecker {
         assert(success);
     }
 
-    // You'll need `ffi=true` in your foundry.toml to run this test
-    // // Remove the `onlyVanillaFoundry`, then run `foundryup-zksync` and then
-    // // `forge test --mt testZkSyncFoundryFails --zksync`
-    // // and this will fail!
-    // function testZkSyncFoundryFails() public onlyVanillaFoundry {
-    //     bool exists = vm.keyExistsJson('{"hi": "true"}', ".hi");
-    //     assert(exists);
-    // }
+    /* You'll need `ffi=true` in your foundry.toml to run this test
+    Remove the `onlyVanillaFoundry`, then run `foundryup-zksync` and then
+    `forge test --mt testZkSyncFoundryFails --zksync`
+    and this will fail!
+    function testZkSyncFoundryFails() public onlyVanillaFoundry {
+        bool exists = vm.keyExistsJson('{"hi": "true"}', ".hi");
+        assert(exists);
+    } */
 }
